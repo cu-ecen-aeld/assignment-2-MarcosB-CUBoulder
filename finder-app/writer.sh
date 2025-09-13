b329dfb1cd05816d68843c049220a3a7540fbacc
Marcos Barrera <marcos.barrera@colorado.edu>
2025-09-13 12:28:43 -0700

    Adds argument parsing and validation
    Implements argument parsing for writefile and writestr.
    
    Adds validation to ensure both arguments are provided, exiting with an error if either is missing.

--- finder-app/writer.sh
+++ finder-app/writer.sh
@@ -1,11 +1,17 @@
 #!/bin/sh
 # Author: Marcos Barrera
 
-#TODO variable writefile that accepts first argument that is a full path to a file including the filename
-
-#TODO variable writestr that accepts a text string which will be written to writefile
-
-#TODO Exit with value 1 error and print statements if any of the arguments are not specified
+#DONE variable writefile that accepts first argument that is a full path to a file including the filename
+writefile = $1
+#DONE variable writestr that accepts second argument a text string which will be written to writefile
+writestr = $2
+#DONE Exit with value 1 error and print statements if any of the arguments are not specified
+if [ -n "${writefile}" ] && [ -n "${writestr}" ]; then
+  echo "passed!"
+else
+  echo "failed: received writefile:${writefile} and writestr:${writestr}"
+  exit 1
+fi
 
 #TODO Creates a new file with name and path writefile overwriting the existing file and creating the path if it doesn't exist
 
