511f97d3ff8281c3dafb5d803aef45698f6218a4
Marcos Barrera <marcos.barrera@colorado.edu>
2025-09-13 09:35:33 -0700

    Add TODOs for finder script

--- finder-app/finder.sh
+++ finder-app/finder.sh
@@ -1,3 +1,15 @@
 #!/bin/sh
 # Description
 # Author: Marcos Barrera
+
+#TODO Accept $1 path to a directory runtime argument as filesdir
+
+#TODO Accept $2 text string to search for as searchstr
+
+#TODO Return value of 1 (error) and print statement if $1 or $2 were not specified
+
+#TODO Return value of 1 (error) if filesdir does not represent a directory
+
+#TODO Prints message "The number of files are X and the number of matching lines are Y"
+# X is the number of files in the directory
+# Y is the number of matching lines found in the files where matching lines refers to a line contained within searchstr
