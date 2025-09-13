6e924b671437ef660397f78f3585ae7cf06b534d
Marcos Barrera <marcos.barrera@colorado.edu>
2025-09-13 10:28:03 -0700

    Accepts command line arguments
    Implements the acceptance of command line arguments for the files directory and search string.
    
    Adds error handling for missing arguments.

--- finder-app/finder.sh
+++ finder-app/finder.sh
@@ -2,12 +2,19 @@
 # Description
 # Author: Marcos Barrera
 
-#TODO Accept $1 path to a directory runtime argument as filesdir
-
-#TODO Accept $2 text string to search for as searchstr
-
-#TODO Return value of 1 (error) and print statement if $1 or $2 were not specified
-
+#DONE Accept $1 path to a directory runtime argument as filesdir
+filesdir=$1
+echo "Filesdir: ${filesdir}"
+#DONE Accept $2 text string to search for as searchstr
+searchstr=$2
+echo "Searchstr: ${searchstr}"
+#DONE Return value of 1 (error) and print statement if $1 or $2 were not specified
+if [ -z "${filesdir}" ] || [ -z "${searchstr}" ]; then
+  echo "failed: received filesdir:${filesdir} and searchstr:${searchstr}"
+  exit 1
+else
+  exit 0
+fi
 #TODO Return value of 1 (error) if filesdir does not represent a directory
 
 #TODO Prints message "The number of files are X and the number of matching lines are Y"
