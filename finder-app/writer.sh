b98eed5c4a6c108faba0580d3fa61647c19015c4
Marcos Barrera <marcos.barrera@colorado.edu>
2025-09-13 13:12:41 -0700

    Creates file and writes string to it
    Implements the functionality to create a file and write a given string to it.
    It also creates the directory path if it doesn't exist.
    Exits with an error message if the file cannot be created.

--- finder-app/writer.sh
+++ finder-app/writer.sh
@@ -6,15 +6,29 @@ writefile=$1
 #DONE variable writestr that accepts second argument a text string which will be written to writefile
 writestr=$2
 #DONE Exit with value 1 error and print statements if any of the arguments are not specified
+
 if [ -n "${writefile}" ] && [ -n "${writestr}" ]; then
-  echo "passed!"
+  #echo "passed!"
+  if [ -d "$(dirname -- ${writefile})" ]; then
+    echo "$(dirname -- ${writefile})"
+    echo "${writestr}" > ${writefile}
+    exit 0
+  elif ! [ -d "$(dirname -- ${writefile})" ]; then
+    mkdir $(dirname -- ${writefile})
+    echo "${writestr}" > ${writefile}
+    exit 0
+  else
+    echo "Error: File can not be created"
+    exit 1
+  fi
+
 else
   echo "failed: received writefile:${writefile} and writestr:${writestr}"
   exit 1
 fi
 
-#TODO Creates a new file with name and path writefile overwriting the existing file and creating the path if it doesn't exist
+#DONE Creates a new file with name and path writefile overwriting the existing file and creating the path if it doesn't exist
 
-#TODO Write content writestr into writefile
+#DONE Write content writestr into writefile
 
-#TODO Exits with value 1 and error print statement if the file could not be created
+#DONE Exits with value 1 and error print statement if the file could not be created
