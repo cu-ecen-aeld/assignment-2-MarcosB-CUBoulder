eadb83a5c53a9990c2c7e0aa54dc4b4430d99323
Marcos Barrera <marcos.barrera@colorado.edu>
2025-09-13 12:09:23 -0700

    Removes and re-adds writer script
    Removes the writer script and then re-adds it. This likely indicates a complete rewrite or a significant change that warranted deleting and re-creating the file in the repository.

--- finder-app/writer.sh
+++ finder-app/writer.sh
@@ -1,3 +1,14 @@
 #!/bin/sh
 # Author: Marcos Barrera
 
+#TODO variable writefile that accepts first argument that is a full path to a file including the filename
+
+#TODO variable writestr that accepts a text string which will be written to writefile
+
+#TODO Exit with value 1 error and print statements if any of the arguments are not specified
+
+#TODO Creates a new file with name and path writefile overwriting the existing file and creating the path if it doesn't exist
+
+#TODO Write content writestr into writefile
+
+#TODO Exits with value 1 and error print statement if the file could not be created
