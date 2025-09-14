3cc303321b618447a093b6584be02a6bab85cf3b
Marcos Barrera <marcos.barrera@colorado.edu>
2025-09-14 11:40:54 -0700

    Writes string to file
    Implements functionality to write a provided string to a file specified by path.
    
    Opens the file in write mode, writes the string, and closes the file.

--- finder-app/writer.c
+++ finder-app/writer.c
@@ -5,9 +5,13 @@ int main(int argc, char *argv[]) {
         printf("Incorrect amount of command line arguments!\n");
         return 1;
     }
+
     //TODO assume that the directory exists and write string input to absolute file path
     //./writer "$WRITEDIR/${username}$i.txt" "$WRITESTR"
     //TODO variable writefile that accepts first argument that is a full path to a file including the filename
+    FILE *writefile = fopen(argv[1], "w");
+    fputs(argv[2], writefile);
+    fclose(writefile);
     //TODO variable writestr that accepts second argument a text string which will be written to writefile
     //TODO Exit with value 1 error and print statements if any of the arguments are not specified
     //TODO Creates a new file with name and path writefile overwriting the existing file and creating the path if it doesn't exist
@@ -15,8 +19,5 @@ int main(int argc, char *argv[]) {
     //TODO Write content writestr into writefile
 
     //TODO Exits with value 1 and error print statement if the file could not be created
-    for (int i=1; i<argc; i++) {
-        printf("%d:%s\n", i, argv[i]);
-    }
     return 0;
 }
