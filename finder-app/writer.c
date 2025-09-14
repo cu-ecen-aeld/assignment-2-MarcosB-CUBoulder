b6805674086fd9b6a0fcf5b55d579f3285f6cee8
Marcos Barrera <marcos.barrera@colorado.edu>
2025-09-14 11:02:08 -0700

    Updates argument handling in main function
    Updates the program to accept two command line arguments,
    prints all arguments with their indices to standard output,
    and updates the error message when an incorrect number of
    arguments is provided.

--- finder-app/writer.c
+++ finder-app/writer.c
@@ -1,11 +1,13 @@
 #include <stdio.h>
 
 int main(int argc, char *argv[]) {
-    if ( argc != 2 ) {
-        printf("No command line arguments!\n");
+    if ( argc != 3 ) {
+        printf("Incorrect amount of command line arguments!\n");
         return 1;
     }
     //TODO assume that the directory exists and write string input to absolute file path
-    printf("argv[1]: %s\n", argv[1]);
+    for (int i=1; i<argc; i++) {
+        printf("%d:%s\n", i, argv[i]);
+    }
     return 0;
 }
