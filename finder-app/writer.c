fbcba37137bb1776017a1e2258f73e3f43c9e508
Marcos Barrera <marcos.barrera@colorado.edu>
2025-09-14 11:11:00 -0700

    Adds TODO comments for writer functionality
    Adds TODO comments outlining the required functionality for the writer application.
    
    The comments detail the expected behavior, including argument handling, file creation/overwriting, and error handling.

--- finder-app/writer.c
+++ finder-app/writer.c
@@ -6,6 +6,15 @@ int main(int argc, char *argv[]) {
         return 1;
     }
     //TODO assume that the directory exists and write string input to absolute file path
+    //./writer "$WRITEDIR/${username}$i.txt" "$WRITESTR"
+    //TODO variable writefile that accepts first argument that is a full path to a file including the filename
+    //TODO variable writestr that accepts second argument a text string which will be written to writefile
+    //TODO Exit with value 1 error and print statements if any of the arguments are not specified
+    //TODO Creates a new file with name and path writefile overwriting the existing file and creating the path if it doesn't exist
+
+    //TODO Write content writestr into writefile
+
+    //TODO Exits with value 1 and error print statement if the file could not be created
     for (int i=1; i<argc; i++) {
         printf("%d:%s\n", i, argv[i]);
     }
