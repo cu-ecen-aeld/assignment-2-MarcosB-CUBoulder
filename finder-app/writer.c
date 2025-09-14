7b8d6e399f08fb0ade20667d45ac304ab56bc301
Marcos Barrera <marcos.barrera@colorado.edu>
2025-09-14 09:23:47 -0700

    Adds basic command line argument handling
    Adds a check for command line arguments, prints an error message if none are provided, and prints the first argument.

--- finder-app/writer.c
+++ finder-app/writer.c
@@ -1,8 +1,11 @@
 #include <stdio.h>
-#include <syslog.h>
-#include <string.h>
 
 int main(int argc, char *argv[]) {
+    if ( argc != 2 ) {
+        printf("No command line arguments!\n");
+        return 1;
+    }
     //TODO assume that the directory exists and write string input to absolute file path
+    printf("argv[1]: %s\n", argv[1]);
     return 0;
 }
